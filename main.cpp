79bd9ecb8338a82c299b474797e1326e8e2e1788
Jordan Akisanya <70146756+Jakisanya@users.noreply.github.com>
2024-03-08 20:45:36 +0000

    general progress

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // --- BlackFilmDatabase_GUI --- \\
 
+#include <iostream>
 #include <QApplication>
 #include <QtWidgets>
 #include <QWidget>
@@ -9,11 +10,109 @@
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QGraphicsVideoItem>
-#include <iostream>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QSlider>
+#include <QCheckBox>
 
-class mainGraphicsView : public QGraphicsView {
+QWidget* createLineEditField(const QString& label, QLineEdit& leWidget, QWidget* widgetContainer) {
+    QHBoxLayout layout(widgetContainer);
+    QLabel fieldLabel(label);
+    layout.addWidget(&fieldLabel);
+    layout.addWidget(&leWidget);
+    return widgetContainer;
+}
+
+QWidget* createComboBoxField(const QString& label, QComboBox& cbWidget, QWidget* widgetContainer) {
+    QHBoxLayout layout(widgetContainer);
+    QLabel fieldLabel(label);
+    layout.addWidget(&fieldLabel);
+    layout.addWidget(&cbWidget);
+    return widgetContainer;
+}
+
+class SearchPage : public QWidget {
 public:
-    mainGraphicsView() {
+    SearchPage(QWidget* parent = nullptr) : QWidget(parent) {
+        QVBoxLayout layout(this);
+
+        // Basic Search Fields
+        layout.addWidget(createLineEditField("Title", titleEdit, titleWidget));
+        layout.addWidget(createLineEditField("Release Year", yearEdit, yearWidget));
+        layout.addWidget(createComboBoxField("Genre", genreCombo, genreWidget));
+        layout.addWidget(createComboBoxField("Film Rating", filmRatingCombo, filmRatingWidget));
+        layout.addWidget(createComboBoxField("Language", languageCombo, languageWidget));
+
+        // Advanced Search Fields (Initially disabled)
+        advancedSearchFields.setEnabled(false);
+        QVBoxLayout advancedLayout(&advancedSearchFields);
+        advancedLayout.addWidget(createLineEditField("IMDB Rating", imdbEdit, imdbRatingWidget));
+        advancedLayout.addWidget(createLineEditField("Rotten Tomatoes Rating", rottenTomatoesEdit, rottenTomatoesRatingWidget));
+        advancedLayout.addWidget(createLineEditField("Keyword", keywordEdit, keywordWidget));
+        advancedLayout.addWidget(createLineEditField("Actor", actorEdit, actorWidget));
+        advancedLayout.addWidget(createLineEditField("Director", directorEdit, directorWidget));
+        advancedLayout.addWidget(createLineEditField("Writer", writerEdit, writerWidget));
+        layout.addWidget(&advancedSearchFields);
+
+        // Advanced Search Checkbox
+        advancedCheckbox.setText("Advanced Search");
+        connect(advancedCheckbox, &QCheckBox::toggled, advancedSearchFields, &QWidget::setEnabled);
+        layout.addWidget(&advancedCheckbox);
+
+        // Search Button
+        searchButton.setText("Search");
+        layout.addWidget(&searchButton);
+
+        setLayout(&layout);
+    }
+
+    // Accessors for search fields
+    [[nodiscard]] QString getTitle() const { return titleEdit.text(); }
+    [[nodiscard]] QString getYear() const { return yearEdit.text(); }
+    [[nodiscard]] QString getGenre() const { return genreCombo.currentText(); }
+    [[nodiscard]] QString getRating() const { return filmRatingCombo.currentText(); }
+    [[nodiscard]] QString getLanguage() const { return languageCombo.currentText(); }
+    [[nodiscard]] QString getIMDBRating() const { return imdbEdit.text(); }
+    [[nodiscard]] QString getRottenTomatoesRating() const { return rottenTomatoesEdit.text(); }
+    [[nodiscard]] QString getKeyword() const { return keywordEdit.text(); }
+    [[nodiscard]] QString getActor() const { return actorEdit.text(); }
+    [[nodiscard]] QString getDirector() const { return directorEdit.text(); }
+    [[nodiscard]] QString getWriter() const { return writerEdit.text(); }
+
+private:
+    QWidget* titleWidget;
+    QWidget* yearWidget;
+    QWidget* genreWidget;
+    QWidget* filmRatingWidget;
+    QWidget* languageWidget;
+    QWidget* imdbRatingWidget;
+    QWidget* rottenTomatoesRatingWidget;
+    QWidget* keywordWidget;
+    QWidget* actorWidget;
+    QWidget* directorWidget;
+    QWidget* writerWidget;
+    QLineEdit titleEdit;
+    QLineEdit yearEdit;
+    QLineEdit imdbEdit;
+    QLineEdit rottenTomatoesEdit;
+    QLineEdit keywordEdit;
+    QLineEdit actorEdit;
+    QLineEdit directorEdit;
+    QLineEdit writerEdit;
+    QComboBox genreCombo;
+    QComboBox filmRatingCombo;
+    QComboBox languageCombo;
+    QPushButton searchButton;
+    QWidget advancedSearchFields;
+    QCheckBox advancedCheckbox;
+};
+
+class MainGraphicsView : public QGraphicsView {
+public:
+    MainGraphicsView() {
         setFixedSize(640, 360);
         setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
         setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
@@ -45,7 +144,7 @@ public:
 
         // Connect button to "Go to Search Page" action
         QObject::connect(&searchButton, &QPushButton::clicked, this,
-                         &mainGraphicsView::showSearchPage);
+                         &MainGraphicsView::showSearchPage);
     }
 
 public slots:
@@ -54,10 +153,29 @@ public slots:
         scene.removeItem(&videoItem);
         scene.removeItem(proxyWidget);
 
-        // Add new content
-        QGraphicsTextItem newTextItem("New Content");
-        newTextItem.setPos(100, 100);
-        scene.addItem(&newTextItem);
+        // Create and display search page
+        searchPage = new SearchPage;
+        scene.addWidget(searchPage);
+
+        // Connect search button to perform search function
+        QObject::connect(searchPage->searchButton, &QPushButton::clicked, this, &MainGraphicsView::performSearch);
+    }
+
+    void performSearch() {
+        // Gather search parameters from the search page
+        QString title = searchPage->getTitle();
+        QString year = searchPage->getYear();
+        QString genre = searchPage->getGenre();
+        QString rating = searchPage->getRating();
+        QString language = searchPage->getLanguage();
+        QString imdbRating = searchPage->getIMDBRating();
+        QString rottenTomatoesRating = searchPage->getRottenTomatoesRating();
+        QString keyword = searchPage->getKeyword();
+        QString actor = searchPage->getActor();
+        QString director = searchPage->getDirector();
+        QString writer = searchPage->getWriter();
+
+        // Implement the search functionality, query the database, and display results
     }
 
 private:
@@ -66,12 +184,12 @@ private:
     QMediaPlayer mediaPlayer;
     QPushButton searchButton;
     QGraphicsProxyWidget* proxyWidget;
+    SearchPage *searchPage;
 };
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    mainGraphicsView view;
-
+    MainGraphicsView view;
     view.show();
     return app.exec();
 }
